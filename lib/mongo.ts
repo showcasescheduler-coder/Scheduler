82e19550a8b913e0112e4612a8c8475e8f57ffeb
thesayf <r_ori@hotmail.co.uk>
2024-10-01 23:14:17 +0100

    attempting to fix bug again

--- lib/mongo.ts
+++ lib/mongo.ts
@@ -1,4 +1,9 @@
 import mongoose from "mongoose";
+import Block from "@/models/Block"; // Import your models
+import Day from "@/models/Day";
+import Task from "@/models/Task";
+import User from "@/models/User";
+import Event from "@/models/Event";
 
 if (!process.env.MONGODB_URI) {
   throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
@@ -28,6 +33,13 @@ async function dbConnect() {
     };
 
     cached.promise = mongoose.connect(uri, opts).then((mongoose) => {
+      // Explicitly compile models
+      mongoose.model("Block", Block.schema);
+      mongoose.model("Day", Day.schema);
+      mongoose.model("Task", Task.schema);
+      mongoose.model("User", User.schema);
+      mongoose.model("Event", Event.schema);
+
       return mongoose;
     });
   }
